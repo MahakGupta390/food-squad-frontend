c1cf8e264ba5f3e260d2c6f850b069e325254134
MahakGupta390 <mahakgupta985@gmail.com>
2026-04-05 01:01:28 +0530

    extra

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './', // Ensures it looks in the current directory
+  build: {
+    outDir: 'dist',
+  }
 })
